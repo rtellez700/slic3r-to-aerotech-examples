af63a6269242faac2dcac1d6fe7554dba08dd78f
Rodrigo Telles <rtellez700@gmail.com>
2023-03-14 16:06:18 -0700

    add reqs.txt

--- utils.py
+++ utils.py
@@ -9,6 +9,12 @@ if os.path.exists('slic3r2aerotech'):
 else:
     os.system('git clone https://github.com/rtellez700/slic3r2aerotech.git')
 
+# if 'slic3r-to-aerotech-examples' not in os.popen('conda env list').read():
+#     os.system('conda create -n slic3r-to-aerotech-examples -y')
+
+# os.system('conda activate slic3r-to-aerotech-examples')
+# os.system('conda install pip -y')
+# os.system('pip install -r requirements.txt -y')
 os.chdir('slic3r2aerotech/src/main/python/')
 os.system('python main.py')
 os.chdir(root_path)
